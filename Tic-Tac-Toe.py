eaa16b7b2cdeb0ec855d94a99f78fe5485559f62
VarunSaraswathibatla <61550359+VarunSaraswathibatla@users.noreply.github.com>
2020-05-16 16:52:05 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,11 +1,10 @@
-"""                                           
-                                                   VARUN TIC-TAC-TOE MANIA
-This program is my Tic-Tac-Toe Game.......
-It has both Single and Two Player options.............
-Try this once and have Fun.....................
+""""                                          WELCOME TO VARUN TIC-TAC-TOE MANIA    
+
+It is my Tic-Tac-Toe game.........
+It has both Single and Two Player Options........
+Try it and have Fun......
+
 """
-
-
 
 
 
@@ -19,7 +18,7 @@ def display(board):
     print("               ",board[6]+' | '+ board[7]+' | '+board[8])
     print("\n")
 
-def player1(p1):
+def player1(p1,userchoice,play1):
     
     z=int(input("%s enter your choice from 1 to 9:"%play1))
     z=z-1
@@ -27,41 +26,22 @@ def player1(p1):
         if(z not in p2):
             if(z not in p1):
                     p1.append(z)
-                    board[z]="O"
-                    display(board)
-            else:
-                print("Space is already occupied....Try again")
-                player1(p1)
-            
-            
-        else:
-            print("Space is already occupied....Try again")
-            player1(p1)
-    else:
-            print("Invalid input:Please try again")
-            player1(p1)
-
-def playersingle(p1,userchoice):
-    
-    z=int(input("%s enter your choice from 1 to 9:"%a))
-    z=z-1
-    if(z>=0 and z<9):
-        if(z not in p2):
-            if(z not in p1):
-                    p1.append(z)
+                    p2.append(z)
                     board[z]=userchoice
                     display(board)
             else:
                 print("Space is already occupied....Try again")
-                playersingle(p1,userchoice)
+                player1(p1,userchoice,play1)
             
             
         else:
             print("Space is already occupied....Try again")
-            playersingle(p1,userchoice)
+            player1(p1,userchoice,play1)
     else:
             print("Invalid input:Please try again")
-            playersingle(p1,userchoice)            
+            player1(p1,userchoice,play1)
+
+
 
 def playercomp(p2,compopt):
     lis=[0,1,2,3,4,5,6,7,8]
@@ -80,27 +60,6 @@ def playercomp(p2,compopt):
 
 
 
-def player2(p2):
-    z=int(input("%s please enter your choice from 1 to 9:"%play2))
-    z=z-1
-    if(z>=0 and z<9):
-        if(z not in p1):
-            if(z not in p2):
-                p2.append(z)
-                board[z]="X"
-               
-                display(board)
-                
-            else:
-                
-                print("Space is already occupied....Try again")
-                player2(p2)
-        else:
-            print("Space is already occupied....Try again")
-            player2(p2)
-    else:
-            print("Invalid input:Please try again")
-            player2(p2)
 
    
 
@@ -130,110 +89,80 @@ def win(board,turn):
 
                                return "O"
                                           
-def gamesingle(turn,board,compopt) :
-     if turn==1:
-        for i in range(4):
-        
-        
-            playersingle(p1,userchoice)
-            if(win(board,turn)=="X"):
-                print("Computer won the match")
-                break
-            elif(win(board,turn)=="O"):
-                print("%s won the match"%a)
-                break
-            playercomp(p2,compopt)
-            if(win(board,turn)=="X"):
-                print("Computer won the match")
-                break
-            elif(win(board,turn)=="O"):
-                print("%s won the match"%a)
-                break
-            elif(win(board,turn)==False):
-                print("Its a Draw!")
-                break
-    
-        else:
-            playersingle(p1,userchoice)
-            if(win(board,turn)=="X"):
-                print("Computer won the match")
-                
-            elif(win(board,turn)=="O"):
-                print("%s won the match"%a)
-                
-            else:
-                print("Its a Draw")
-        
-    
-     else:
-        for i in range(4):
-        
-               playercomp(p2,compopt)
-               if(win(board,turn)=="X"):
-                       print("%s won the match"%a)
-                       break
-               elif(win(board,turn)=="O"):
-                         print("Computer won the match")
-                         break
-               playersingle(p1,userchoice)
-               if(win(board,turn)=="X"):
-                       print("%s won the match"%a)
-                       break
-               elif(win(board,turn)=="O"):
-                         print("Computer won the match")
-                         break
-               elif(win(board,turn)==False):
-                    print("Its a Draw!")
+def gamesingle(turn,board,compopt,userchoice) :
+   if turn==1:
+       play1=a
+       for i in range(4):
+           player1(p1,"O",play1)
+           if win(board,turn)=="O":
+                    print("%s is winner"%play1)
                     break
-        else:
            playercomp(p2,compopt)
-           if(win(board,turn)=="X"):
-                       print("%s won the match"%a)
-    
-    
-           elif(win(board,turn)=="O"):
-                         print("Computer won the match")
-                         
-
+           if win(board,turn)=="X":
+                    print("Computer won the match")
+                    break
+       else:
+           player1(p1,"O",play1)
+           if win(board,turn)=="O":
+                    print("%s is winner"%play1)
            else:
                print("Its a Draw")
+            
+
+   else:
+        play1=a
+        for i in range(4):
+               playercomp(p2,compopt)
+               
+               if win(board,turn)=="O":
+                        print("Computer won the match")
+                        break
+               player1(p1,"X",play1)
+               if win(board,turn)=="X":
+                    print("%s is winner"%play1)
+                    break
+
+        else:
+            playercomp(p2,compopt)
+               
+            if win(board,turn)=="O":
+                        print("Computer won the match")
+                        
+            else:
+                print("Its Draw")
+
+
+
+
+
+
+
+
+       
 
 
 def game(turn,board):
-    
-
-        for i in range(4):
-        
-        
-            player1(p1)
-            if(win(board,turn)=="X"):
-                print("%s won the match"%play2)
+    if turn==1:
+        play1,play2=a,b
+    else:
+         play1,play2=b,a
+    for i in range(4):
+            player1(p1,"O",play1)
+            if win(board,turn)=="O":
+                print("%s is winner"%play1)
                 break
-            elif(win(board,turn)=="O"):
-                print("%s won the match"%play1)
+            player1(p2,"X",play2)
+            if win(board,turn)=="X":
+                print("%s is winner"%play2)
                 break
-            player2(p2)
-            if(win(board,turn)=="X"):
-                print("%s won the match"%play2)
-                break
-            elif(win(board,turn)=="O"):
-                print("%s won the match"%play1)
-                break
-            elif(win(board,turn)==False):
-                print("Its a Draw!")
-                break
+    else:   
+          player1(p1,"O",play1)
+          if win(board,turn)=="O":
+                print("%s is winner"%play1)
+          else:
+              print("Its Draw")
          
-        else:
-            player1(p1)
-            if(win(board,turn)=="X"):
-                print("%s won the match"%play2)
-                
-            elif(win(board,turn)=="O"):
-                print("%s won the match"%play1)
-                
-            else:
-                print("Its a Draw")
-              
+    
             
   
 print("                              Welcome to  Varun Tic-Tac-Toe Mania                                                     ")
@@ -245,29 +174,25 @@ for w in range(100):
             opt=input("Wanna play Single or Two Player(S/T): ")
             if (opt=="T" or opt=="t"):
                         b=input("Enter your friend's name: ")
-                        if(a==b):
-                            print("Player names can't be same.....Try giving different player names....")
-                            continue
+                        p1=[]
+                        p2=[]               
+                        ch1=input("Enter your choice(X/O): ")
+                        if ch1=="X" or ch1=="x":
+                            print("So",b," option will be 'O' so he will start first")
+                            turn=0
+                            play1,play2=b,a
+                            
+                            
+                            
                         else:
-                                    p1=[]
-                                    p2=[]               
-                                    ch1=input("Enter your choice(X/O): ")
-                                    if ch1=="X" or ch1=="x":
-                                        print("So",b," option will be 'O' so he will start first")
-                                        turn=0
-                                        play1,play2=b,a
-                                        
-                                        
-                                        
-                                    else:
-                                        print("So",b, "option will be 'X' so you will start first")
-                                        turn=1
-                                        play1,play2=a,b
+                            print("So",b, "option will be 'X' so you will start first")
+                            turn=1
+                            play1,play2=a,b
 
 
-                                        
-                                    display(board)
-                                    game(turn,board)
+                            
+                        display(board)
+                        game(turn,board)
             else:
                 b="Computer"
                 p1=[]
@@ -280,7 +205,7 @@ for w in range(100):
                       turn=1
                       compopt="X"
                       userchoice="O"
-                      gamesingle(turn,board,compopt)
+                      gamesingle(turn,board,compopt,userchoice)
                       
                 else:
                      print("So",b," option will be 'O' he  will start first...")
@@ -290,7 +215,7 @@ for w in range(100):
                      userchoice="X"
                      turn=0
                      
-                     gamesingle(turn,board,compopt) 
+                     gamesingle(turn,board,compopt,userchoice) 
             playagain=input("Wanna Try Again.......(Y/N): ")
             if playagain=="Y" or playagain=="y":
                 continue
