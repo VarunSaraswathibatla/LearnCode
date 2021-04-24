8add4a407bbad7ce3693489c2dcb2514f83ec034
VarunSaraswathibatla <61550359+VarunSaraswathibatla@users.noreply.github.com>
2021-04-24 10:20:19 +0530

    Huffman Decoding

---
+++ HuffmanDecoding.java
@@ -0,0 +1,42 @@
+/*https://www.hackerrank.com/challenges/tree-huffman-decoding/problem*/
+
+/* Complete the following decide method*/
+
+	void decode(String s, Node root) 
+    {
+        // Start your code here
+    
+        StringBuilder ans= new StringBuilder();
+        Node temp=root;
+        for(char i: s.toCharArray())
+        {
+            if(i=='1')
+            {
+                temp=temp.right;
+                if(temp.data=='\0')
+                {
+                    continue;
+                }
+                else
+                {
+                    ans.append(temp.data);
+                    temp=root;
+                    
+                }
+            }
+            if(i=='0')
+            {
+                temp=temp.left;
+                if(temp.data=='\0')
+                {
+                    continue;
+                }
+                else
+                {
+                    ans.append(temp.data);
+                    temp=root;
+                } 
+            }
+        }
+        System.out.println(ans); 
+    }
